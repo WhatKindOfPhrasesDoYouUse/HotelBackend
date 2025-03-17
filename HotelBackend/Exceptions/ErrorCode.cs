3055f5bb2523c0872be1c8772ed82964c334775a
Dima Oshuev <mazafon64@gmail.com>
2025-03-17 13:39:18 +0300

    добавил классы для обработки ошибок сервиса

---
+++ HotelBackend/Exceptions/ErrorCode.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBackend.Exceptions
+{
+    public enum ErrorCode
+    {
+        NotFound = 1,
+        BadRequest = 2,
+        InternalServerError = 3,
+        Unauthorized = 4
+    }
+}
---
+++ HotelBackend/Exceptions/ServiceException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HotelBackend.Exceptions
+{
+    public class ServiceException : Exception
+    {
+        public ErrorCode ErrorCode { get; set; }
+
+        public ServiceException(ErrorCode errorCode, string message) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public ServiceException(ErrorCode errorCode, string message, Exception innerException) : base(message, innerException)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
