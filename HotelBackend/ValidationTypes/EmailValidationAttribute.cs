64697593cbc63fc934334f2f51d72aa579bd03d9
Dima Oshuev <mazafon64@gmail.com>
2025-03-17 12:38:18 +0300

    добавил валидирующие классы

---
+++ HotelBackend/ValidationTypes/EmailValidationAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace HotelBackend.ValidationTypes
+{
+    public class EmailValidationAttribute : ValidationAttribute
+    {
+        private static readonly Regex EmailRegex = new(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string email)
+            {
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    return new ValidationResult("Поле Email является обязательным.");
+                }
+
+                if (!EmailRegex.IsMatch(email))
+                {
+                    return new ValidationResult("Неверный формат email.");
+                }
+
+                if (email.Length > 100)
+                {
+                    return new ValidationResult("Длина email не может превышать 100 символов.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ HotelBackend/ValidationTypes/NameValidationAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace HotelBackend.ValidationTypes
+{
+    public class NameValidationAttribute : ValidationAttribute
+    {
+        private static readonly Regex NameRegex = new(@"^[A-Za-zА-Яа-я\-]+$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private const int MaxLength = 50;
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string name)
+            {
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    return new ValidationResult("Поле является обязательным.");
+                }
+
+                if (!NameRegex.IsMatch(name))
+                {
+                    return new ValidationResult("Поле должно содержать только буквы (русские или латинские) и дефис.");
+                }
+
+                if (name.Length > MaxLength)
+                {
+                    return new ValidationResult($"Длина поля не может превышать {MaxLength} символов.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
