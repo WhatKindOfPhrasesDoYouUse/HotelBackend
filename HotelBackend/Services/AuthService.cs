6c390fcaac5e3d940049c70011787977daab84d0
Dima Oshuev <mazafon64@gmail.com>
2025-03-19 01:22:40 +0300

    добавил id клиента в claim токена

--- HotelBackend/Services/AuthService.cs
+++ HotelBackend/Services/AuthService.cs
@@ -30,7 +30,8 @@ namespace HotelBackend.Services
             var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Sub, client.Email),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("client_id", client.Id.ToString())
             };
 
             using (var context = new ApplicationDbContext())
