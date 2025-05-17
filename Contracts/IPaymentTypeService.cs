e98117b6380bdf5ee43bda738fcc9439315151a2
Dima Oshuev <mazafon64@gmail.com>
2025-05-17 18:16:17 +0300

    добавил CRUD для типов оплаты

--- Contracts/IPaymentTypeService.cs
+++ Contracts/IPaymentTypeService.cs
@@ -5,5 +5,9 @@ namespace HotelBackend.Contracts
     public interface IPaymentTypeService
     {
         Task<IEnumerable<PaymentType>> GetAllPaymentTypes();
+        Task SavePaymentType(PaymentType paymentType);
+        Task DeletePaymentTypeById(long paymentTypeId);
+        Task<PaymentType> GetPaymentTypeById(long paymentTypeId);
+        Task UpdatePaymentTypeById(long paymentTypeId, PaymentType newPaymentType);
     }
 }
--- Controllers/PaymentTypeController.cs
+++ Controllers/PaymentTypeController.cs
@@ -1,5 +1,7 @@
 ﻿using HotelBackend.Contracts;
 using HotelBackend.Exceptions;
+using HotelBackend.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelBackend.Controllers
@@ -19,6 +21,82 @@ namespace HotelBackend.Controllers
             {
                 var paymentTypes = await _paymentTypeService.GetAllPaymentTypes();
                 return StatusCode(200, paymentTypes);
+            }
+            catch (ServiceException ex)
+            {
+                return StatusCode((int)ex.ErrorCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Произошла внутренняя ошибка сервера: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> SavePaymentType(PaymentType paymentType)
+        {
+            try
+            {
+                await _paymentTypeService.SavePaymentType(paymentType);
+                return StatusCode(201, "Тип оплаты успешно создан");
+            }
+            catch (ServiceException ex)
+            {
+                return StatusCode((int)ex.ErrorCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Произошла внутренняя ошибка сервера: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{paymentTypeId:long}")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> DeletePaymentTypeById(long paymentTypeId)
+        {
+            try
+            {
+                await _paymentTypeService.DeletePaymentTypeById(paymentTypeId);
+                return StatusCode(200, "Тип оплаты успешно удален");
+            }
+            catch (ServiceException ex)
+            {
+                return StatusCode((int)ex.ErrorCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Произошла внутренняя ошибка сервера: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{paymentTypeId:long}")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> GetPaymentTypeById(long paymentTypeId)
+        {
+            try
+            {
+                var paymentType = await _paymentTypeService.GetPaymentTypeById(paymentTypeId);
+                return StatusCode(200, paymentType);
+            }
+            catch (ServiceException ex)
+            {
+                return StatusCode((int)ex.ErrorCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Произошла внутренняя ошибка сервера: {ex.Message}");
+            }
+        }
+
+        [HttpPatch("{paymentTypeId:long}")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> UpdatePaymentTypeById(long paymentTypeId, PaymentType newPaymentType)
+        {
+            try
+            {
+                await _paymentTypeService.UpdatePaymentTypeById(paymentTypeId, newPaymentType);
+                return StatusCode(200, "Тип оплаты успешно обновлен");
             }
             catch (ServiceException ex)
             {
